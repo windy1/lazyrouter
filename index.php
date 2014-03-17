b17c07feb069921514006038c546dab794b663c9
Walker Crouse <walkercrouse@hotmail.com>
2014-03-17 19:32:37 -0400

    simplify example
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ require_once 'src/lazyrouter/Router.php';
 use lazyrouter\Router;
 
 $router = new Router();
-
-$failCallback = function() {
+$router->route(function() {
     http_response_code(404);
     die();
-};
-
-$router->route($failCallback, function() {});
+}, function() {});
